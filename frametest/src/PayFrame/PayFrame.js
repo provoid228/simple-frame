32dce489c5055a4691003113319bebc1d7178783
provoid228 <provoid228@gmail.com>
2024-03-09 21:25:28 +0300

    added PayFrame

---
+++ frametest/src/PayFrame/PayFrame.js
@@ -0,0 +1,32 @@
+import { FrameMetadata } from '@coinbase/onchainkit';
+
+ export default function PayFrame() {
+  return (
+  
+    <FrameMetadata
+      buttons={[
+        {
+          label: 'Tell me the story',
+        },
+        {
+          action: 'link',
+          label: 'Link to Google',
+          target: 'https://www.google.com'
+        },
+        {
+          action: 'post_redirect',
+          label: 'Redirect to cute pictures',
+        },
+      ]}
+      image={{
+       src: 'https://zizzamia.xyz/park-3.png',
+       aspectRatio: '1:1'
+      }}
+      input={{
+        text: 'Tell me a boat story',
+      }}
+      postUrl="https://zizzamia.xyz/api/frame"
+    />
+    
+  );
+}
